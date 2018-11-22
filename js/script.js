252626d4d420b0a31df4ef0680da869422a7849f
Etienne BLANC-COQUAND <etienne.blanc-coquand@kgp-agency.com>
2018-11-22 17:06:25 +0100

    Finish by id

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 $(document).ready(function () {
+  const div = {
+    // searchByName: $("#by-name"),
+    searchById: $("#by-id"),
+    pokemonName: $("#pokemon-name"),
+    pokemonId: $("#pokemon-id"),
+    resultsContainer: $("#results-container"),
+    lastSeenData: $("last-seen-data"),
+    lastSeenName: $("last-seen-name"),
+    lastSeenId: $("last-seen-id")
+  }
+  const buttons = {
+    searchById: $("#button-find-pokemon-by-id"),
+    // searchByName: $("#button-find-pokemon-by-name"),
+    resultsReturn: $("#button-results-return")
+  }
   const divSearchByName = $("#by-name");
   const divSearchById = $("#by-id");
   const divResultsContainer = $("#results-container");
+  const divPokemonName = $("#pokemon-name");
+  const divPokemonId = $("#pokemon-id");
   const buttonSearchByName = $("#button-find-pokemon-by-name");
   const buttonSearchById = $("#button-find-pokemon-by-id");
   const buttonResultsReturn = $("#button-results-return");
@@ -12,46 +29,59 @@ $(document).ready(function () {
     baseUrlSearchType: 'https://pokeapi.co/api/v2/type/',
     baseUrlSearchAbility: 'https://pokeapi.co/api/v2/ability/'
   };
-  
-  /*
-  $.ajax({
-    type: "GET",
-    url: "https://pokeapi.co/api/v2/pokemon/1",
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-    }
-  });
-  */
-
   /**
    * Action click on the button to search Pokemon by name
    */
-  buttonSearchByName.click(function (e) { 
+  /*
+  buttonSearchByName.click(function (e) {
     console.info('Future feature');
     alert('Bientot disponible en France');
   });
+  */
 
   /**
    * Action click on the button to search Pokemon by id
    */
   buttonSearchById.click(function (e) {
+    // Hide or show different divs
     divSearchById.addClass('hide');
     divSearchByName.addClass('hide');
+    $("#last-seen-data").addClass('hide');
     divResultsContainer.removeClass('hide');
-    let pokemonId = null;
-    pokemonId = Number(inputSearchById.val());
-    if (pokemonId === null || pokemonId === 0) {
+    let pokemonId = Number(inputSearchById.val())
+    // Set the url to be more readable
+    let urlPokemon = urls.baseUrlSearchPokemon + pokemonId;
+    // Test if the input is empty
+    if (pokemonId === null || pokemonId === 0 || pokemonId === '') {
       alert('Veuillez renseigner le numéro d\'identification d\'un Pokemon');
     } else {
-
+      // Retrieve all data
+      $.getJSON(urlPokemon,
+        function (data, textStatus, jqXHR) {
+          // Write data in the html
+          divPokemonName.html(data.name);
+          divPokemonId.html(data.id);
+          // Remove old pokemon seen
+          localStorage.removeItem('lastIdSeen');
+          localStorage.removeItem('lastNameSeen');
+          // Replace by the fresh one
+          localStorage.setItem('lastIdSeen', data.id);
+          localStorage.setItem('lastNameSeen', data.name);
+        }
+      );
     }
   });
 
-  buttonResultsReturn.click(function (e) { 
+  buttonResultsReturn.click(function (e) {
     divSearchById.removeClass('hide');
     divSearchByName.removeClass('hide');
     divResultsContainer.addClass('hide');
+    divPokemonName.html('');
+    divPokemonId.html('');
+    if (localStorage.getItem('lastIdSeen') !== undefined && localStorage.getItem('lastIdSeen') !== null && localStorage.getItem('lastIdSeen') !== '' && localStorage.getItem('lastNameSeen') !== undefined && localStorage.getItem('lastNameSeen') !== null && localStorage.getItem('lastNameSeen') !== '') {
+      $("#last-seen-data").removeClass('hide');
+      $("#last-seen-name").html(localStorage.getItem('lastNameSeen'));
+      $("#last-seen-id").html(localStorage.getItem('lastIdSeen'));
+    }
   });
-
 });
